638e93143c02aff101b28c4fa730eff66a7e85f7
RC_Student_lab <tokollowill99@gmail.com>
2025-04-22 15:56:33 +0200

    Fixed padding

--- app/src/main/java/com/example/pennywise/activity_add_entry.kt
+++ app/src/main/java/com/example/pennywise/activity_add_entry.kt
@@ -53,6 +53,13 @@ class activity_add_entry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scrollView)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         setContentView(R.layout.activity_add_entry)
 
         initViews()
